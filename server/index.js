aecdad62070ea333a3f73d6e3bb696e385be3460
1t1e1 <1t1ebir@gmail.com>
2020-02-21 15:26:18 +0300

    data basic

--- server/index.js
+++ server/index.js
@@ -4,6 +4,39 @@ import cors from "cors";
 const app = express();
 
 app.use(cors());
+const data = [
+    {
+        id: 0,
+        kitapAdi: "Gulun adi",
+        yazarinAdi: "umberto eco",
+        yayinEvi: "Can Yayinlari",
+    },
+    {
+        id: 1,
+        kitapAdi: "Gulun adi1",
+        yazarinAdi: "umberto eco1",
+        yayinEvi: "Can Yayinlari",
+    },
+    {
+        id: 2,
+        kitapAdi: "Gulun adi2",
+        yazarinAdi: "umberto eco2",
+        yayinEvi: "Can Yayinlari",
+    },
+    {
+        id: 3,
+        kitapAdi: "Gulun adi3",
+        yazarinAdi: "umberto eco3",
+        yayinEvi: "Can Yayinlari",
+    },
+    {
+        id: 4,
+        kitapAdi: "Gulun adi4",
+        yazarinAdi: "umberto eco4",
+        yayinEvi: "Can Yayinlari",
+    },
+];
+
 app.get("/", (req, res) => {
     res.send("Root");
 });
