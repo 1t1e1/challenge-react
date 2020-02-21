53fdad0c7b464085b5eb09c4589dd7268d10940a
1t1e1 <1t1ebir@gmail.com>
2020-02-21 15:17:53 +0300

    express basic routers

---
+++ README.md
@@ -0,0 +1,3 @@
+curl http://localhost:3000/create -X POST
+curl http://localhost:3000/detail/21
+curl http://localhost:3000/
--- server/index.js
+++ server/index.js
@@ -1,9 +1,19 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 
+app.use(cors());
 app.get("/", (req, res) => {
     res.send("Root");
+});
+
+app.get("/detail/:id", (req, res) => {
+    res.send("detail = " + req.params.id);
+});
+
+app.post("/create/", (req, res) => {
+    res.send("post req on /create/");
 });
 
 app.listen(3000, () => console.log("Example app listening on port 3000!"));
