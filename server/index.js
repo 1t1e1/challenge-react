fa381e0ee7b501b9da37d0e4321eae801a8c1ad1
1t1e1 <1t1ebir@gmail.com>
2020-02-21 16:12:48 +0300

    data outside

--- server/index.js
+++ server/index.js
@@ -1,44 +1,13 @@
 import express from "express";
 import cors from "cors";
 import uuidv4 from "uuid/v4";
+import data from "./src/data";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-const data = {
-    0: {
-        id: 0,
-        kitapAdi: "Gulun adi",
-        yazarinAdi: "umberto eco",
-        yayinEvi: "Can Yayinlari",
-    },
-    1: {
-        id: 1,
-        kitapAdi: "Gulun adi1",
-        yazarinAdi: "umberto eco1",
-        yayinEvi: "Can Yayinlari",
-    },
-    2: {
-        id: 2,
-        kitapAdi: "Gulun adi2",
-        yazarinAdi: "umberto eco2",
-        yayinEvi: "Can Yayinlari",
-    },
-    3: {
-        id: 3,
-        kitapAdi: "Gulun adi3",
-        yazarinAdi: "umberto eco3",
-        yayinEvi: "Can Yayinlari",
-    },
-    4: {
-        id: 4,
-        kitapAdi: "Gulun adi4",
-        yazarinAdi: "umberto eco4",
-        yayinEvi: "Can Yayinlari",
-    },
-};
 
 app.get("/", (req, res) => {
     res.send(data);
---
+++ server/src/data/index.js
@@ -0,0 +1,34 @@
+let kitaplik = {
+    0: {
+        id: 0,
+        kitapAdi: "Gulun adi",
+        yazarinAdi: "umberto eco",
+        yayinEvi: "Can Yayinlari",
+    },
+    1: {
+        id: 1,
+        kitapAdi: "Gulun adi1",
+        yazarinAdi: "umberto eco1",
+        yayinEvi: "Can Yayinlari",
+    },
+    2: {
+        id: 2,
+        kitapAdi: "Gulun adi2",
+        yazarinAdi: "umberto eco2",
+        yayinEvi: "Can Yayinlari",
+    },
+    3: {
+        id: 3,
+        kitapAdi: "Gulun adi3",
+        yazarinAdi: "umberto eco3",
+        yayinEvi: "Can Yayinlari",
+    },
+    4: {
+        id: 4,
+        kitapAdi: "Gulun adi4",
+        yazarinAdi: "umberto eco4",
+        yayinEvi: "Can Yayinlari",
+    },
+};
+
+export default kitaplik;
