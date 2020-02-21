e742e15b365945bda54f4c527567e1cd0ebf8840
1t1e1 <1t1ebir@gmail.com>
2020-02-21 15:46:21 +0300

    basic root, create & detail

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
-curl http://localhost:3000/create -X POST
-curl http://localhost:3000/detail/21
-curl http://localhost:3000/
+
+## root page
+```
+curl http://localhost:3000/        
+{"0":{"id":0,"kitapAdi":"Gulun adi","yazarinAdi":"umberto eco","yayinEvi":"Can Yayinlari"},"1":{"id":1,"kitapAdi":"Gulun adi1","yazarinAdi":"umberto eco1","yayinEvi":"Can Yayinlari"},"2":{"id":2,"kitapAdi":"Gulun adi2","yazarinAdi":"umberto eco2","yayinEvi":"Can Yayinlari"},"3":{"id":3,"kitapAdi":"Gulun adi3","yazarinAdi":"umberto eco3","yayinEvi":"Can Yayinlari"},"4":{"id":4,"kitapAdi":"Gulun adi4","yazarinAdi":"umberto eco4","yayinEvi":"Can Yayinlari"}}%
+```
+
+## detail page
+```
+curl http://localhost:3000/detail/1
+{"id":1,"kitapAdi":"Gulun adi1","yazarinAdi":"umberto eco1","yayinEvi":"Can Yayinlari"}%
+```
+
+## create page
+
+```
+╰─➤ curl http://localhost:3000/create -X POST  
+{"id":"af43eabf-35d5-48bd-91ba-0ffee9f3f8d1","kitapAdi":"deneme","yazarinAdi":"deneme","yayinEvi":"deneme"}%                                                    ╭
+╰─➤  curl http://localhost:3000/detail/af43eabf-35d5-48bd-91ba-0ffee9f3f8d1
+{"id":"af43eabf-35d5-48bd-91ba-0ffee9f3f8d1","kitapAdi":"deneme","yazarinAdi":"deneme","yayinEvi":"deneme"}%                                                    ╭
+```
--- server/index.js
+++ server/index.js
@@ -1,52 +1,61 @@
 import express from "express";
 import cors from "cors";
+import uuidv4 from "uuid/v4";
 
 const app = express();
 
 app.use(cors());
-const data = [
-    {
+const data = {
+    0: {
         id: 0,
         kitapAdi: "Gulun adi",
         yazarinAdi: "umberto eco",
         yayinEvi: "Can Yayinlari",
     },
-    {
+    1: {
         id: 1,
         kitapAdi: "Gulun adi1",
         yazarinAdi: "umberto eco1",
         yayinEvi: "Can Yayinlari",
     },
-    {
+    2: {
         id: 2,
         kitapAdi: "Gulun adi2",
         yazarinAdi: "umberto eco2",
         yayinEvi: "Can Yayinlari",
     },
-    {
+    3: {
         id: 3,
         kitapAdi: "Gulun adi3",
         yazarinAdi: "umberto eco3",
         yayinEvi: "Can Yayinlari",
     },
-    {
+    4: {
         id: 4,
         kitapAdi: "Gulun adi4",
         yazarinAdi: "umberto eco4",
         yayinEvi: "Can Yayinlari",
     },
-];
+};
 
 app.get("/", (req, res) => {
-    res.send("Root");
+    res.send(data);
 });
 
 app.get("/detail/:id", (req, res) => {
-    res.send("detail = " + req.params.id);
+    res.send(data[req.params.id]);
 });
 
 app.post("/create/", (req, res) => {
-    res.send("post req on /create/");
+    const id = uuidv4();
+    const yeniKitap = {
+        id,
+        kitapAdi: "deneme",
+        yazarinAdi: "deneme",
+        yayinEvi: "deneme",
+    };
+    data[id] = yeniKitap;
+    res.send(yeniKitap);
 });
 
 app.listen(3000, () => console.log("Example app listening on port 3000!"));
